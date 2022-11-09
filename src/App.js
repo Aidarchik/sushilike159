5807144844602f1902f64f0af234ca155c116eea
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-11-09 23:11:12 +0500

    наведение мыши

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div className="p-6 max-w-sm mx-auto bg-white-500 hover:bg-slate-100 rounded-xl shadow-lg flex items-center space-x-4">
       <div className="shrink-0">
         <img className="h-12 w-12" src="./logo192.png" alt="ChitChat Logo" />
       </div>
