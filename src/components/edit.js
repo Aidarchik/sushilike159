90b22f1918184b1698913312dfc869a4972985c7
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-12-14 21:32:34 +0500

    edit height

--- src/components/edit.js
+++ src/components/edit.js
@@ -5,8 +5,8 @@ const Edit = () => {
         <>
             <div className="fixed container mx-auto max-w-3xl bottom-0 left-0 right-0">
                 <div className="flex">
-                    <input className='w-full text-sm px-10 py-1 focus:outline-none bg-gray-900 text-white opacity-90' type="text" placeholder='Text' />
-                    <button className='px-2 text-white bg-blue-400 rounded-tr-lg rounded-br-lg hover:bg-blue-500'>Отправить</button>
+                    <input className='h-12 w-full text-lg px-10 py-1 focus:outline-none bg-gray-900 text-white opacity-90' type="text" placeholder='Text' />
+                    <button className='px-2 text-white text-lg bg-blue-400 rounded-tr-lg rounded-br-lg hover:bg-blue-500'>Отправить</button>
                 </div>
             </div>
         </>
