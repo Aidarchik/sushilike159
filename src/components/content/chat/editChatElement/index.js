76a812bec656bcdd4c05f653d1ca137885120b13
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2022-12-29 00:35:03 +0500

    fixed consts

--- src/components/content/chat/editChatElement/index.js
+++ src/components/content/chat/editChatElement/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { URL_POSTS, URL_POSTS_REMOTE, URL_POSTS_REMOTE2 } from '../../../../consts'
+import { URL_POSTS_REMOTE } from '../../../../consts'
 import Card from './card'
 
 const EditChatElement = () => {
