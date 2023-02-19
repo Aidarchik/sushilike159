6094e1ef3ffa048f74b98361f04ba38dfd93f098
Aidarchik <62675384+Aidarchik@users.noreply.github.com>
2023-02-19 16:08:51 +0500

    consts

--- src/consts/const.js
+++ src/consts/const.js
@@ -1,2 +1,3 @@
 export const URL_POSTS = 'http://localhost:5000/api/posts'
-export const URL_POSTS_REMOTE = 'https://sushilike159.ru/api/posts'
+export const URL_POSTS_REMOTE = 'http://localhost:5000/api/posts'
+// export const URL_POSTS_REMOTE = 'https://sushilike159.ru/api/posts'
